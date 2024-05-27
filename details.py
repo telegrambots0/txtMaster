4588b8f219c07f9f06c87d8dd6a69a42200e55b7
telegrambots0 <146921432+telegrambots0@users.noreply.github.com>
2024-05-27 13:55:28 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 27815405
-api_hash = "4e70821cd2af3322f7cf2f2887e32821"
-bot_token = "7078184845:AAEHj8NIzn7-kGJXumQgAGVlhDnhrhf6uB8"
-sudo_groups = -1002141190229
+api_id = 25263708
+api_hash = "9bde48b267ce65a576b478c0ff8c7bbb"
+bot_token = "7073327110:AAGFGVPzzqquAQNMBXACtyZRv5UuVvOIGgg"
+sudo_groups = -1002137091035
